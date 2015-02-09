8d007edbc23f67e6f4f0aa3af0fc50338e18d131
Davy Engone <engone.davy@kbc.be>
2015-02-09 16:58:46 +0100

    added training modules: views, controllers, services

---
+++ public/modules/trainings/controllers/training.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('trainings').controller('TrainingsController', [TrainingsController]);
+
+function TrainingsController(){
+	this.get = function(){
+		console.log('not implemented yet!');
+	};
+}
